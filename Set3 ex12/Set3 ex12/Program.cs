1526d9d4ab76ac8ad3361a75939a0745e91e74f6
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-12 16:12:57 +0200

    Add files via upload

---
+++ Set3 ex12/Set3 ex12/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Set3_ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti elementul de pe pozitia {i}:");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (v[i] > v[j])
+                        (v[i], v[j]) = (v[j], v[i]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(v[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
