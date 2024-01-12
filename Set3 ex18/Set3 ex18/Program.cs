e028717f07245efd3b0f3a8e93fcb53942643c63
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-12 16:16:43 +0200

    Add files via upload

---
+++ Set3 ex18/Set3 ex18/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Set3_ex18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gradul polinomului este: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Punctul in care se calculeaza valoarera polinomului este: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n+1];
+
+            int P = 0;
+            for(int i = 0; i <= n; i++) 
+            {
+                Console.Write($"Introduceti coeficientul lui x^{i}: ");
+                v[i] = int.Parse(Console.ReadLine());
+                P += v[i] * (int)Math.Pow(x, i);
+            }
+
+            Console.WriteLine($"Valoarea polinomului in punctul {x} este: {P}.");
+        }
+    }
+}
