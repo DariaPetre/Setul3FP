699ad8835846b69ffb81f7b86e73eb606873df3b
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-12 16:17:31 +0200

    Add files via upload

---
+++ Set3 ex19/Set3 ex19/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Set3_ex19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n1=");
+            int n1 = int.Parse(Console.ReadLine());
+
+            int[] s = new int[n1];
+
+            for (int i = 0; i < n1; i++)
+            {
+                Console.Write($"Introduceti elementul de pe pozitia {i} din vectorul s: ");
+                s[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("n2=");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int[] p = new int[n1];
+
+            for (int i = 0; i < n2; i++)
+            {
+                Console.Write($"Introduceti elementul de pe pozitia {i} din vectorul p: ");
+                p[i] = int.Parse(Console.ReadLine());
+            }
+
+            int cnt = 0;
+            for(int i = 0; i <= n1-n2; i++)
+            {
+                bool egale = true;
+                for(int j = 0; j < n2; j++)
+                {
+                    if (s[i + j] != p[j])
+                    {
+                        egale = false;
+                        break;
+                    }
+                }
+
+                if (egale)
+                    cnt++;
+            }
+
+            Console.WriteLine($"Vectorul p apare de {cnt} ori in s.");
+          
+        }
+    }
+}
