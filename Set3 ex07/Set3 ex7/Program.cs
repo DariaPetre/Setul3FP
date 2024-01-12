84a637364431c8c0e745431c6fc88278756bb2d2
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-12 16:10:07 +0200

    Add files via upload

---
+++ Set3 ex07/Set3 ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Set3_ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+
+            int[] v = new int[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}:");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i <= (n-1) / 2; i++)
+            {
+                (v[i], v[n - i - 1]) = (v[n - i - 1], v[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+        }
+    }
+}
