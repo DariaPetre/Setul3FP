65d61008b690ae8742fdab6aa1274516e87165e6
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-12 16:12:25 +0200

    Add files via upload

---
+++ Set3 ex11/Set3 ex11/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Set3_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] ciur = new int[n+1];
+
+            for(int i = 2; i <= n; i++)
+            {
+                if (ciur[i] == 0)
+                {
+                    for(int j = 2 * i; j <= n; j+=i)
+                        ciur[j] = 1;
+                }
+            }
+            if (n == 1)
+                Console.WriteLine();
+            else
+            {
+                for(int i = 2; i <= n; i++)
+                {
+                    if (ciur[i] == 0)
+                        Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
